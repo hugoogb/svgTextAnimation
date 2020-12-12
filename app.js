12a410e0f3b4d4745c8ec74d0b9bebcd6dd01ac5
Hugoo GB <hugogaben8.02@gmail.com>
2020-12-12 16:39:59 +0100

    added all project files

---
+++ app.js
@@ -0,0 +1,28 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    function animateSgv(id, delay, delayIncrement) {
+      const logo = document.getElementById(id);
+      const logoPaths = document.querySelectorAll(`#${id} path`);
+      delay = delay;
+      for (let i = 0; i < logoPaths.length; i++) {
+        //console.log(logoPaths[i].getTotalLength());
+        logoPaths[i].style.strokeDasharray = logoPaths[i].getTotalLength();
+        logoPaths[i].style.strokeDashoffset = logoPaths[i].getTotalLength();
+        logoPaths[i].style.animation = `line-anim 2s ease forwards ${delay}s`;
+        delay += delayIncrement;
+        console.log(delay);
+      }
+      logo.style.animation = `fill 0.5s ease forwards ${delay}s`;
+    }
+    animateSgv("logo", 0, 0.5);
+  },
+  false
+);
+
+window.addEventListener("load", function () {
+  document.querySelector(".loader").classList.add("opacity-0");
+  setTimeout(function () {
+    document.querySelector(".loader").style.display = "none";
+  }, 6000);
+});
